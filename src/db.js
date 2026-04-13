39aac3711ea710e9ed27bf4d22fd592ce3c06d34
veristein <veristeintech@gmail.com>
2026-04-13 09:51:17 -0400

    changes after deployment strategy update

--- src/db.js
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { DB_URL } from "../drizzle.config.js";
-const db = drizzle(DB_URL);
-export default db;
-//# sourceMappingURL=db.js.map
