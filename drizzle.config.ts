942451e1e7ee0734f5c71ddc2bcfc67b5819ca0b
veristein <veristeintech@gmail.com>
2026-04-13 09:49:13 -0400

    changes after deployment strategy update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,15 +4,11 @@ import { defineConfig } from "drizzle-kit";
 export const isProduction =
   process.env.NODE_ENV === "production" || process.env.NODE_ENV === "prod";
 
-export const DB_URL = isProduction
-  ? process.env.DB_URL_PROD!
-  : process.env.DB_URL_LOCAL!;
-
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: DB_URL!,
+    url: process.env.DB_URL!,
   },
 });
--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { DB_URL } from "../drizzle.config.js";
 
-const db = drizzle(DB_URL!);
+const db = drizzle(process.env.DB_URL!);
 
 export default db;
